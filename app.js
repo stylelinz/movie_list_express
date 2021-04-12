a34e499a97e261cc943581e582f3f90c66e1213d
StyleLinz_Man <cwchuang1997@gmail.com>
2021-04-12 22:12:27 +0800

    add: render movies by loop #20210412003

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const movieLists = require('./movies.json')
 
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// Apply static resources
+app.use(express.static('public'))
+
 // Route settings
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { movies: movieLists.results })
 })
 
 app.listen(port, () => {
